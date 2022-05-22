058d1e408057c5c0ab213539db90fe94e04421c1
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-05-22 18:23:05 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -4,9 +4,18 @@ import typewise_alert
 
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
-    self.assertFalse(typewise_alert.check_and_alert('TO_EMAIL', 'MED_ACTIVE_COOLING', 40) == 'TOO_LOW')
-    self.assertFalse(typewise_alert.check_and_alert('TO_EMAIL', 'HI_ACTIVE_COOLING', 45) == 'TOO_LOW')
-    self.assertFalse(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 100) == 'TOO_LOW')
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'MED_ACTIVE_COOLING', -1) == 'TOO_LOW')
+    self.assertFalse(typewise_alert.check_and_alert('TO_EMAIL', 'MED_ACTIVE_COOLING', 0) == 'TOO_LOW')
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'MED_ACTIVE_COOLING', 40) == 'NORMAL')
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'MED_ACTIVE_COOLING', 45) == 'TOO_HIGH')
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'HI_ACTIVE_COOLING', -1) == 'TOO_LOW')
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'HI_ACTIVE_COOLING', 45) == 'NORMAL')
+    self.assertFalse(typewise_alert.check_and_alert('TO_EMAIL', 'HI_ACTIVE_COOLING', 40) == 'TOO_LOW')
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'HI_ACTIVE_COOLING', 50) == 'TOO_HIGH')
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', -1) == 'TOO_LOW')
+    self.assertFalse(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 0) == 'TOO_LOW')
+    self.assertFalse(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 35) == 'TOO_LOW')
+    self.assertFalse(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 40) == 'TOO_LOW')
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
 
 
