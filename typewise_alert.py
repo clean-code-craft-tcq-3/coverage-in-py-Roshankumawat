d75e0bb0caa480969b8974db8338a996094bb934
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-05-22 15:19:42 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,4 +1,4 @@
-batteryChar={'PASSIVE_COOLING':[0,35], 'HI_ACTIVE_COOLING':[0,45],'MED_ACTIVE_COOLING':[0,40]}
+batteryChar=dict({'PASSIVE_COOLING':[0,35], 'HI_ACTIVE_COOLING':[0,45],'MED_ACTIVE_COOLING':[0,40]})
 
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
